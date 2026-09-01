494d678ed42ff176c55892d496ba533419948442
marcoshernad23 <marcoshernad23@gmail.com>
2026-08-31 21:25:21 -0300

    Guia 3 completa

--- especificaciones_guia3.md
+++ especificaciones_guia3.md
@@ -101,6 +101,8 @@ problema digitoDecenas (x: Z) : Z {
 
 }
 
+Ejercicio 4
+
 problema productoInterno ((x1, y1), (x2, y2) : R) : R {
 
     requiere : { True }
@@ -131,4 +133,110 @@ problema sumaTerna ((x, y, z): R) : R {
     
     asegura : { res = x+y+z }
     
+}
+
+problema sumarSoloMultiplos ((x, y, z), n: R) : R {
+
+    requiere : { True }
+    
+    asegura : { (f(x)+f(y)+f(z)) con f(p) = p si p ≡ 0 (n) o 0 en caso contrario }
+    
+}
+
+problema posPrimerPar ((x, y, z): R) : R {
+
+    requiere : { True }
+    
+    asegura : { x ≡ 0 (2) -> res = 1}
+
+    asegura : { y ≡ 0 (2) -> res = 2}
+    
+    asegura : { z ≡ 0 (2) -> res = 3}
+
+    asegura : { res = 4 }
+    
+}
+
+problema crearPar (x, y: R) : (R, R) {
+
+    requiere : { True }
+
+    asegura: { (x, y) }
+
+}
+
+problema invertir ((x, y): R) : (R, R) {
+
+    requiere : { True }
+
+    asegura: { (y, x) }
+
+}
+
+Ejercicio 9
+
+a) Devuelve 0 si el número es, ironicamente, distinto de 0, en cuyo caso devuelve 1
+
+problema f1 (x : R) : R {
+
+    requiere : { True }
+
+    asegura: { x ≠ 0 -> res = 0 }
+
+    asegura: { x = 0 -> res = 1 }
+
+}
+
+b) Toma un signo y lo multiplica por 15
+
+problema f2 (x : R) : R {
+
+    requiere : { True }
+
+    asegura: { x = 1 -> res = 15 }
+
+    asegura: { x = -1 -> res = -15 }
+
+}
+
+c) Transforma todos los números menores a 9 en 7, e intenta transformar todos los números mayores a 3 en 5, solo consiguiendolo con aquellos que son tambien mayores a 9
+
+problema f3 (x : R) : R {
+
+    requiere : { True }
+
+    asegura: { x <= 9 -> res = 7 }
+
+    asegura: { x >= 3 -> res = 5 }
+
+}
+
+d) Devuelve el promedio entre dos numeros
+
+problema f4 (x, y : R) : R {
+
+    requiere : { True }
+
+    asegura: { res = (x+y)/2 }
+
+}
+
+e) Devuelve el promedio entre los valores de un par
+
+problema f5 ((x,y) : R) : R {
+
+    requiere : { True }
+
+    asegura: { res = (x+y)/2 }
+
+}
+
+f) Confirma si la parte entera del número racional es igual al entero
+
+problema f6 (x : R, y : N) : R {
+
+    requiere : { True }
+
+    asegura: { parte entera de x == y -> res = True }
+
 }
--- guia3.hs
+++ guia3.hs
@@ -1,17 +1,17 @@
 {- HLINT ignore "Eta reduce" -}
 
 -- Ej 1
--- a)
+-- a
 f :: Integer -> Integer
 f 1 = 8
 f 4 = 131
 f 16 = 16
--- b)
+-- b
 g :: Integer -> Integer
 g 8 = 16
 g 16 = 4
 g 131 = 1
--- c)
+-- c
 h :: Integer -> Integer
 h n = f (g n)
 
@@ -110,9 +110,8 @@ digitoDecenas x = div (mod x 100)  10
 -- 3
 
 estanRelacionados :: Integer -> Integer -> Bool
-estanRelacionados n1 n2
-    | mod n1 n2 /= 0 = False
-    | otherwise = True
+estanRelacionados n1 n2 = mod n1 n2 == 0
+
 
 -- Por qué? La relación esta formada por a^2 + a*b*k = 0 con a, b y k /= 0
 -- Si intento despejar k => a^2 = -a*b*k => a^2/a = -b*k => a = -b*k => a/b = -k
@@ -138,3 +137,94 @@ distancia (x1, y1) (x2, y2) = sqrt((x2-x1)^2 + (y2-y1)^2 )
 
 sumaTerna :: (Integer, Integer, Integer) -> Integer
 sumaTerna (x,y,z) = x+y+z
+
+-- e
+
+boolToInt :: Bool -> Integer
+boolToInt b
+    | not b = 0
+    | otherwise = 1
+
+sumarSoloMultiplos :: (Integer, Integer, Integer) -> Integer -> Integer
+sumarSoloMultiplos (x,y,z) n = x * boolToInt (esMultiploDe x n) + y * boolToInt (esMultiploDe y n) + z * boolToInt (esMultiploDe z n)
+
+-- f
+
+posPrimerPar :: (Integer, Integer, Integer) -> Integer
+posPrimerPar (x,y,z)
+    | even x = 1
+    | even y = 2
+    | even z = 3
+    | otherwise = 4
+
+-- g
+
+crearPar :: a -> b -> (a,b)
+crearPar a b = (a, b)
+
+-- h
+
+invertir :: (a,b) -> (b,a)
+invertir (a,b) = (b,a)
+
+-- i
+
+type Punto2D =  (Float, Float)
+
+productoInterno' :: Punto2D -> Punto2D -> Float
+productoInterno' (x1,y1) (x2,y2) = x1*x2 + y1*y2
+
+esParMenor' :: Punto2D -> Punto2D -> Bool
+esParMenor' (x1, y1) (x2, y2) = x1 < x2 && y1 < y2
+
+distancia' :: Punto2D -> Punto2D -> Float
+distancia' (x1, y1) (x2, y2) = sqrt((x2-x1)^2 + (y2-y1)^2 )
+
+-- 5
+
+f' :: Integer -> Integer
+f' n
+    | n <= 7 = n^2
+    | otherwise = 2*n-1
+
+g' :: Integer -> Integer
+g' n
+    | even n = n `div` 2
+    | otherwise = 3*n+1
+
+todosMenores :: (Integer, Integer, Integer) -> Bool
+todosMenores (x,y,z) = f' x > g' x && f' y > g' y && f' z > g' z
+
+-- 6
+
+type Anio = Integer
+type EsBisiesto = Bool
+
+bisiesto :: Anio -> EsBisiesto
+bisiesto y = mod y 4 == 0 || (mod y 100 == 0 && mod y 400 == 0)
+
+-- 7 
+
+abs' :: Float -> Float
+abs' n
+    | n < 0 = (-1)*n
+    | otherwise = n
+
+distanciaManhattan :: (Float, Float, Float) -> (Float, Float, Float) -> Float
+distanciaManhattan (x1,y1,z1) (x2,y2,z2) = abs' (x1-x2) + abs' (y1-y2) + abs' (z1-z2)
+
+type Punto3D = (Float, Float, Float)
+
+distanciaManhattan' :: Punto3D -> Punto3D -> Float
+distanciaManhattan' (x1,y1,z1) (x2,y2,z2) = abs' (x1-x2) + abs' (y1-y2) + abs' (z1-z2)
+
+-- 8
+
+sumaUltimosDosDigitos :: Integer -> Integer
+sumaUltimosDosDigitos x = mod x 10 + mod (div x 10) 10
+
+comparar :: Integer -> Integer -> Integer
+comparar x y
+    | sumaUltimosDosDigitos x < sumaUltimosDosDigitos y = 1
+    | sumaUltimosDosDigitos x > sumaUltimosDosDigitos y = -1
+    | otherwise = 0
